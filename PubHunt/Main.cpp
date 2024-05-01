0138ee4734177a8c7e6aefd03ce7e246e7b0ab1e
Chail35 <146902363+Chail35@users.noreply.github.com>
2024-05-01 15:58:51 -0400

    Update Main.cpp

--- PubHunt/Main.cpp
+++ PubHunt/Main.cpp
@@ -26,19 +26,19 @@ bool should_exit = false;
 // ----------------------------------------------------------------------------
 
 void printUsage() {
-
-	printf("PubHunt [-check] [-h] [-v] \n");
-	printf("        [-gi GPU ids: 0,1...] [-gx gridsize: g0x,g0y,g1x,g1y, ...]\n");
-	printf("        [-o outputfile] [inputFile]\n\n");
-	printf(" -v                       : Print version\n");
-	printf(" -gi gpuId1,gpuId2,...    : List of GPU(s) to use, default is 0\n");
-	printf(" -gx g1x,g1y,g2x,g2y, ... : Specify GPU(s) kernel gridsize, default is 8*(MP number),128\n");
-	printf(" -o outputfile            : Output results to the specified file\n");
-	printf(" -l                       : List cuda enabled devices\n");
-	printf(" -check                   : Check Int calculations\n");
-	printf(" inputFile                : List of the hash160, one per line in hex format (text mode)\n\n");
-	exit(0);
-
+    printf("PubHunt [-check] [-h] [-v] \n");
+    printf("        [-gi GPU ids: 0,1...] [-gx gridsize: g0x,g0y,g1x,g1y, ...]\n");
+    printf("        [-o outputfile] [-sr startRange] [-er endRange] [inputFile]\n\n");
+    printf(" -v                       : Print version\n");
+    printf(" -gi gpuId1,gpuId2,...    : List of GPU(s) to use, default is 0\n");
+    printf(" -gx g1x,g1y,g2x,g2y, ... : Specify GPU(s) kernel gridsize, default is 8*(MP number),128\n");
+    printf(" -o outputfile            : Output results to the specified file\n");
+    printf(" -sr startRange           : Start of the key range to search (in hex)\n");
+    printf(" -er endRange             : End of the key range to search (in hex)\n");
+    printf(" -l                       : List cuda enabled devices\n");
+    printf(" -check                   : Check Int calculations\n");
+    printf(" inputFile                : List of the hash160, one per line in hex format (text mode)\n\n");
+    exit(0);
 }
 // ----------------------------------------------------------------------------
 
@@ -46,167 +46,175 @@ void printUsage() {
 #ifdef WIN64
 BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
 {
-	switch (fdwCtrlType) {
-	case CTRL_C_EVENT:
-		//printf("\n\nCtrl-C event\n\n");
-		should_exit = true;
-		return TRUE;
+    switch (fdwCtrlType) {
+    case CTRL_C_EVENT:
+        //printf("\n\nCtrl-C event\n\n");
+        should_exit = true;
+        return TRUE;
 
-	default:
-		return TRUE;
-	}
+    default:
+        return TRUE;
+    }
 }
 #else
 void CtrlHandler(int signum) {
-	printf("\n\nBYE\n");
-	exit(signum);
+    printf("\n\nBYE\n");
+    exit(signum);
 }
 #endif
 
 int main(int argc, const char* argv[])
 {
-	// Global Init
-	Timer::Init();
-	rseed(Timer::getSeed32());
+    // Global Init
+    Timer::Init();
+    rseed(Timer::getSeed32());
 
-	srand(time(NULL));
+    srand(time(NULL));
 
-	// for Int check
-	Int P, order;
-	P.SetBase16("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F");
-	order.SetBase16("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141");
-	Int::InitK1(&order);
-	Int::SetupField(&P);
-	//
+    // for Int check
+    Int P, order;
+    P.SetBase16("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F");
+    order.SetBase16("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141");
+    Int::InitK1(&order);
+    Int::SetupField(&P);
+    //
 
-	vector<int> gpuId = { 0 };
-	vector<int> gridSize;
+    uint64_t startRange = 0;
+    uint64_t endRange = 0;
+    vector<int> gpuId = { 0 };
+    vector<int> gridSize;
 
-	string outputFile = "Found.txt";
+    string outputFile = "Found.txt";
 
-	std::vector<std::vector<uint8_t>> inputHashes;
+    std::vector<std::vector<uint8_t>> inputHashes;
 
 
-	int a = 1;
+    int a = 1;
 
-	while (a < argc) {
-		if (strcmp(argv[a], "-gi") == 0) {
-			a++;
-			getInts("gi", gpuId, string(argv[a]), ',');
-			a++;
-		}
-		else if (strcmp(argv[a], "-v") == 0) {
-			printf("%s\n", RELEASE);
-			exit(0);
-		}
-		else if (strcmp(argv[a], "-check") == 0) {
-
-			Int::Check();
+    while (a < argc) {
+        if (strcmp(argv[a], "-gi") == 0) {
+            a++;
+            getInts("gi", gpuId, string(argv[a]), ',');
+            a++;
+        }
+        else if (strcmp(argv[a], "-v") == 0) {
+            printf("%s\n", RELEASE);
+            exit(0);
+        }
+        else if (strcmp(argv[a], "-check") == 0) {
+            Int::Check();
 #ifdef WITHGPU
-			if (gridSize.size() == 0) {
-				gridSize.push_back(-1);
-				gridSize.push_back(128);
-			}
+            if (gridSize.size() == 0) {
+                gridSize.push_back(-1);
+                gridSize.push_back(128);
+            }
 #else
-			printf("GPU code not compiled, use -DWITHGPU when compiling.\n");
+            printf("GPU code not compiled, use -DWITHGPU when compiling.\n");
 #endif
-			exit(0);
-		}
-		else if (strcmp(argv[a], "-l") == 0) {
-
+            exit(0);
+        }
+        else if (strcmp(argv[a], "-sr") == 0) {
+            a++;
+            startRange = strtoull(argv[a], nullptr, 16);
+            a++;
+        }
+        else if (strcmp(argv[a], "-er") == 0) {
+            a++;
+            endRange = strtoull(argv[a], nullptr, 16);
+            a++;
+        }
+        else if (strcmp(argv[a], "-l") == 0) {
 #ifdef WITHGPU
-			GPUEngine::PrintCudaInfo();
+            GPUEngine::PrintCudaInfo();
 #else
-			printf("GPU code not compiled, use -DWITHGPU when compiling.\n");
+            printf("GPU code not compiled, use -DWITHGPU when compiling.\n");
 #endif
-			exit(0);
+            exit(0);
+        }
+        else if (strcmp(argv[a], "-gx") == 0) {
+            a++;
+            getInts("gridSize", gridSize, string(argv[a]), ',');
+            a++;
+        }
+        else if (strcmp(argv[a], "-o") == 0) {
+            a++;
+            outputFile = string(argv[a]);
+            a++;
+        }
+        else if (strcmp(argv[a], "-h") == 0) {
+            printUsage();
+        }
+        else if (a == argc - 1) {
+            parseFile(string(argv[a]), inputHashes);
+            a++;
+        }
+        else {
+            printf("Unexpected %s argument\n", argv[a]);
+            exit(-1);
+        }
+    }
 
-		}
-		else if (strcmp(argv[a], "-gx") == 0) {
-			a++;
-			getInts("gridSize", gridSize, string(argv[a]), ',');
-			a++;
-	}
-		else if (strcmp(argv[a], "-o") == 0) {
-			a++;
-			outputFile = string(argv[a]);
-			a++;
-		}
-		else if (strcmp(argv[a], "-h") == 0) {
-			printUsage();
-		}
-		else if (a == argc - 1) {
-			parseFile(string(argv[a]), inputHashes);
-			a++;
-		}
-		else {
-			printf("Unexpected %s argument\n", argv[a]);
-			exit(-1);
-		}
+    if (gridSize.size() == 0) {
+        for (int i = 0; i < gpuId.size(); i++) {
+            gridSize.push_back(-1);
+            gridSize.push_back(128);
+        }
+    }
+    else if (gridSize.size() != gpuId.size() * 2) {
+        printf("Invalid gridSize or gpuId argument, must have coherent size\n");
+        exit(-1);
+    }
 
-}
-
-	if (gridSize.size() == 0) {
-		for (int i = 0; i < gpuId.size(); i++) {
-			gridSize.push_back(-1);
-			gridSize.push_back(128);
-		}
-	}
-	else if (gridSize.size() != gpuId.size() * 2) {
-		printf("Invalid gridSize or gpuId argument, must have coherent size\n");
-		exit(-1);
-	}
-
-	printf("\n");
-	printf("PubHunt v" RELEASE "\n");
-	printf("\n");
-	printf("DEVICE       : %s\n", "GPU");
-	if (1) {
-		printf("GPU IDS      : ");
-		for (int i = 0; i < gpuId.size(); i++) {
-			printf("%d", gpuId.at(i));
-			if (i + 1 < gpuId.size())
-				printf(", ");
-		}
-		printf("\n");
-		printf("GPU GRIDSIZE : ");
-		for (int i = 0; i < gridSize.size(); i++) {
-			printf("%d", gridSize.at(i));
-			if (i + 1 < gridSize.size()) {
-				if ((i + 1) % 2 != 0) {
-					printf("x");
-				}
-				else {
-					printf(", ");
-				}
-			}
-		}
-		printf("\n");
-	}
-	printf("NUM HASH160  : %llu\n", inputHashes.size());
-	printf("OUTPUT FILE  : %s\n", outputFile.c_str());
+    printf("\n");
+    printf("PubHunt v" RELEASE "\n");
+    printf("\n");
+    printf("DEVICE       : %s\n", "GPU");
+    if (1) {
+        printf("GPU IDS      : ");
+        for (int i = 0; i < gpuId.size(); i++) {
+            printf("%d", gpuId.at(i));
+            if (i + 1 < gpuId.size())
+                printf(", ");
+        }
+        printf("\n");
+        printf("GPU GRIDSIZE : ");
+        for (int i = 0; i < gridSize.size(); i++) {
+            printf("%d", gridSize.at(i));
+            if (i + 1 < gridSize.size()) {
+                if ((i + 1) % 2 != 0) {
+                    printf("x");
+                }
+                else {
+                    printf(", ");
+                }
+            }
+        }
+        printf("\n");
+    }
+    printf("NUM HASH160  : %" PRIu64 "\n", static_cast<uint64_t>(inputHashes.size()));
+    printf("OUTPUT FILE  : %s\n", outputFile.c_str());
+	printf("KEY RANGE    : 0x%016llx - 0x%016llx\n", startRange, endRange);
 
 #ifdef WIN64
-	if (SetConsoleCtrlHandler(CtrlHandler, TRUE)) {
+    if (SetConsoleCtrlHandler(CtrlHandler, TRUE)) {
+        PubHunt* v = new PubHunt(inputHashes, outputFile, startRange, endRange);
 
-		PubHunt* v = new PubHunt(inputHashes, outputFile);
-
-		v->Search(gpuId, gridSize, should_exit);
-		delete v;
-		printf("\n\nBYE\n");
-		return 0;
-	}
-	else {
-		printf("Error: could not set control-c handler\n");
-		return 1;
-	}
+        v->Search(gpuId, gridSize, should_exit);
+        delete v;
+        printf("\n\nBYE\n");
+        return 0;
+    }
+    else {
+        printf("Error: could not set control-c handler\n");
+        return 1;
+    }
 #else
-	signal(SIGINT, CtrlHandler);
+    signal(SIGINT, CtrlHandler);
 
-	PubHunt* v = new PubHunt(inputHashes, outputFile);
+    PubHunt* v = new PubHunt(inputHashes, outputFile, startRange, endRange);
 
-	v->Search(gpuId, gridSize, should_exit);
-	delete v;
-	return 0;
+    v->Search(gpuId, gridSize, should_exit);
+    delete v;
+    return 0;
 #endif
 }
